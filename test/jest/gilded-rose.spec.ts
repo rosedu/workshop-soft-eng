4015f364c981fc2f77bedec4ab51d5ed8af13b01
MadlenaB <madlenablajin@yahoo.com>
2024-11-09 12:43:27 +0200

    my solution for the task
    Signed-off-by: MadlenaB <madlenablajin@yahoo.com>

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -3,7 +3,7 @@ import { Item, GildedRose } from '@/gilded-rose';
 describe('Gilded Rose', () => {
     it('should foo', () => {
         // Arrange
-        const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+        const gildedRose = new GildedRose([new Item('bar', 0, 0)]);
 
         // Act
         const items = gildedRose.updateQuality();
@@ -20,6 +20,17 @@ describe('Gilded Rose', () => {
       const items = gildedRose.updateQuality();
 
       // Assert
-      expect(items[0].quality).toBe(1);
+      expect(items[0].quality).toBe(0);
+    })
+
+    it('aged brie doesnt drop by 150', () => {
+      // Arrange
+      const gildedRose = new GildedRose([new Item('Aged Brie', 1, 149)]);
+
+      // Act
+      let items = gildedRose.updateQuality();
+      items = gildedRose.updateQuality();
+      // Assert
+      expect(items[0].quality).toBe(151);
     })
 });
