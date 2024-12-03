62c02b48676ce20d94593dfa36ce14a4d43543d9
matei-birleanu <dolhasca001@gmail.com>
2024-12-03 17:53:06 +0200

    Final

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -3,7 +3,7 @@ import { Item, GildedRose } from '@/gilded-rose';
 describe('Gilded Rose', () => {
     it('should foo', () => {
         // Arrange
-        const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+        const gildedRose = new GildedRose([new Item('bar', 0, 0)]);
 
         // Act
         const items = gildedRose.updateQuality();
@@ -20,6 +20,149 @@ describe('Gilded Rose', () => {
       const items = gildedRose.updateQuality();
 
       // Assert
-      expect(items[0].quality).toBe(1);
+      expect(items[0].quality).toBe(0);
     })
+    it('test1', () => {
+      // Arrange
+      const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 40)]);
+
+      // Act
+      const items = gildedRose.updateQuality();
+
+      // Assert
+      expect(items[0].quality).toBe(42);
+  });
+  it('test2', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 10, 40)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].quality).toBe(40);
+  });
+  it('test3', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Aged Brie', 10, 40)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].quality).toBe(41);
+  });
+  it('test4', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Aged Brie', 1, 40)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].quality).toBe(41);
+  });
+  it('test5', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Elixir of the Mongoose', 1, 40)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].quality).toBe(39);
+  });
+  it('test6', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Conjured Mana Cake', 1, 40)]);
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].quality).toBe(39);
+  });
+  it('test7', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Dexterity Vest', 1, 40)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].quality).toBe(39);
+  });
+  it('test8', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Elixir of the Mongoose', -1 , 40)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].quality).toBe(38);
+  });
+  it('test9', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 40)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].sellIn).toBe(9);
+  });
+  it('test10', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Elixir of the Mongoose', 10, 40)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].sellIn).toBe(9);
+  });
+  it('test11', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Conjured Mana Cake', 10, 40)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].sellIn).toBe(9);
+  });
+  
+  it('test12', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Aged Brie', 10, 40)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].sellIn).toBe(9);
+  });
+
+  it('test13', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Dexterity Vest', 10, 40)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].sellIn).toBe(9);
+  });
+
+  it('test14', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Aged Brie', 10, 40)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].quality).toBe(41);
+  });
+
 });
