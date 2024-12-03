c730e3426439363f3fd3bb28ffaacfe13b810bef
OctaVianu8 <stanescu.matei.octavian@gmail.com>
2024-12-03 18:00:12 +0200

    tst

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -5,7 +5,7 @@ export class Item {
 
     constructor(name, sellIn, quality) {
         this.name = name;
-        this.sellIn = sellIn;
+        this.sellIn =  sellIn;
         this.quality = quality;
     }
 }
