12ef7328ae573b49c566f305e36c6909fe797236
Stefan19K <stefanalexandru589@gmail.com>
2024-12-03 22:02:45 +0200

    fix quantity calculation bug

--- app/gilded-rose-refactor.ts
+++ app/gilded-rose-refactor.ts
@@ -23,14 +23,13 @@ export class GildedRose {
             if (this.items[i].name == 'Aged Brie')
             {                
                 this.items[i].sellIn -= 1
-                
-                if (this.items[i].sellIn < 0)
-                    this.items[i].quality += 2
-                else
-                    this.items[i].quality += 1
 
-                if (this.items[i].quality > 50)
-                    this.items[i].quality = 50
+                if (this.items[i].quality < 50)
+                    this.items[i].quality += 1
+                
+                if (this.items[i].sellIn < 0 &&
+                    this.items[i].quality < 50)
+                    this.items[i].quality += 1
             }
 
             if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert')
@@ -42,13 +41,15 @@ export class GildedRose {
                 else
                     if (this.items[i].quality < 50)
                     {
-                        if (this.items[i].sellIn < 6)
-                            this.items[i].quality += 3
-                        else if (this.items[i].sellIn < 11)
-                            this.items[i].quality += 2
-        
-                        if (this.items[i].quality > 50)
-                            this.items[i].quality = 50
+                        this.items[i].quality += 1
+
+                        if (this.items[i].sellIn < 11 &&
+                            this.items[i].quality < 50)
+                            this.items[i].quality += 1
+                        
+                        if (this.items[i].sellIn < 6 &&
+                            this.items[i].quality < 50)
+                            this.items[i].quality += 1
                     }
             }
 
--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -36,7 +36,7 @@ describe('Gilded Rose', () => {
 
     it('name == \'Aged Brie\'', () => {
       // Arrange
-      const gildedRose = new GildedRose([new Item('Aged Brie', 1, 49), new Item('Aged Brie', 0, 49)]);
+      const gildedRose = new GildedRose([new Item('Aged Brie', 1, 49), new Item('Aged Brie', 0, 48), new Item('Aged Brie', 0, 70)]);
 
       // Act
       const items = gildedRose.updateQuality();
@@ -44,18 +44,22 @@ describe('Gilded Rose', () => {
       // Assert
       expect(items[0].quality).toBe(50);
       expect(items[1].quality).toBe(50);
+      expect(items[2].quality).toBe(70);
     })
 
     it('name == \'Backstage passes to a TAFKAL80ETC concert\'', () => {
       // Arrange
-      const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 48), new Item('Backstage passes to a TAFKAL80ETC concert', 7, 49)]);
+      const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 47), 
+                                         new Item('Backstage passes to a TAFKAL80ETC concert', 7, 45), 
+                                         new Item('Backstage passes to a TAFKAL80ETC concert', 7, 70)]);
 
       // Act
       const items = gildedRose.updateQuality();
 
       // Assert
       expect(items[0].quality).toBe(50);
-      expect(items[1].quality).toBe(50);
+      expect(items[1].quality).toBe(47);
+      expect(items[2].quality).toBe(70);
     })
 
     it('name == \'Sulfuras, Hand of Ragnaros\'', () => {
