3e8951cb06c933694ddbcf554441232b3bd0bd21
cosminstercu <cosminstercu@gmail.com>
2025-04-06 12:26:33 +0300

    added(tests) written new tests in /test/jest/gilded-rose.spec.ts

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -22,4 +22,102 @@ describe('Gilded Rose', () => {
       // Assert
       expect(items[0].quality).toBe(0);
     })
+
+    it('Quality degrading 2x after sell date passed', () => {
+      // Arrange
+      const gildedRose = new GildedRose([new Item('Sword', 2, 10)]);
+
+      // Act
+      const items = gildedRose.updateQuality();
+
+      let days: number = 4;
+      if (process.argv.length > 2) {
+          days = +process.argv[2];
+      }
+
+      for (let i = 0; i < days + 1; i++) {
+          gildedRose.updateQuality();
+      }
+
+
+      // Assert
+      expect(items[0].quality).toBe(4);
+    })
+
+    it('Item quality is never negative', () => {
+      // Arrange
+      const gildedRose = new GildedRose([new Item('Sword', 2, -1)]);
+
+      // Act
+      const items = gildedRose.updateQuality();
+
+      // Assert
+      expect(items[0].quality).toBeGreaterThanOrEqual(0);
+    })
+
+    it('Item quality is always less than 50', () => {
+      // Arrange
+      const gildedRose = new GildedRose([new Item('Sword', 2, 55), new Item('Sulfuras item', 2, 80)]);
+
+      // Act
+      const items = gildedRose.updateQuality();
+
+      // Assert
+      //Sulfuras is allowed to have quality > 50 (ALways 80, never alters)
+      expect(items[1].quality).toBe(80);
+      expect(items[0].quality).toBeLessThan(50);
+    })
+
+    it('Aged brie increases in quality', () => {
+      // Arrange
+      const gildedRose = new GildedRose([new Item('Aged Brie', 2, 5)]);
+
+      // Act
+      const items = gildedRose.updateQuality();
+
+      // Assert
+      expect(items[0].quality).toBe(6);
+      expect(items[1].quality).toBe(9);
+    })
+
+    it('Sulfuras never decreases in quality / is never sold', () => {
+      // Arrange
+      const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 2, 80)]);
+
+      // Act
+      const items = gildedRose.updateQuality();
+
+      // Assert
+      expect(items[0].sellIn).toBe(2);
+      expect(items[0].quality).toBe(80);
+    })
+
+    it('Backstage passes quality changes differently', () => {
+      // Arrange
+      const gildedRose = new GildedRose([new Item('Backstage passes item', 8, 80),
+                                         new Item('Backstage passes item', 2, 80),
+                                         new Item('Backstage passes item', 0, 80),
+      ]);
+
+      // Act
+      const items = gildedRose.updateQuality();
+
+      // Assert
+      expect(items[0].quality).toBe(82);
+      expect(items[1].quality).toBe(83);
+      expect(items[0].quality).toBe(0);
+      
+    })
+
+    it('Conjured items degrade 2x in quality', () => {
+      // Arrange
+      const gildedRose = new GildedRose([new Item('Conjured Item', 8, 80)]);
+
+      // Act
+      const items = gildedRose.updateQuality();
+
+      // Assert
+      expect(items[0].quality).toBe(78);      
+    })
+
 });
