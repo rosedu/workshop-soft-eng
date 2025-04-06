163b290dadb80d8fface9890546caa2df45bdf6c
kittykatspiral <anaflorentinaneacsu@gmail.com>
2025-04-06 12:40:55 +0300

    Fixed and added tests; refactored updateQuality function

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -9,7 +9,7 @@ describe('Gilded Rose', () => {
         const items = gildedRose.updateQuality();
 
         // Assert
-        expect(items[0].name).toBe('bar');
+        expect(items[0].name).toBe('foo');
     });
 
     it('sword quality drops by 1', () => {
@@ -20,6 +20,67 @@ describe('Gilded Rose', () => {
       const items = gildedRose.updateQuality();
 
       // Assert
-      expect(items[0].quality).toBe(1);
-    })
+      expect(items[0].quality).toBe(0);
+    });
+    it('conjured quality drops by 2', () => {
+        // Arrange
+        const gildedRose = new GildedRose([new Item('Conjured Mana Cake', 7, 4)]);
+
+        // Act
+        const items = gildedRose.updateQuality();
+
+        // Assert
+        expect(items[0].quality).toBe(2);
+    });
+  it('sulfuras quality does not drop', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 5, 80)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].quality).toBe(80);
+  });
+  it('sulfuras sellIn does not decrease', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 5, 80)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].sellIn).toBe(5);
+  });
+  it('sword sellIn decreases', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('sword', 7, 17)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].sellIn).toBe(6);
+  });
+  it('brie quality limit at 50', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Aged Brie', 7, 50)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].quality).toBe(50);
+  });
+  it('backstage quality zero after concert', () => {
+    // Arrange
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 50)]);
+
+    // Act
+    const items = gildedRose.updateQuality();
+
+    // Assert
+    expect(items[0].quality).toBe(0);
+  });
+
 });
