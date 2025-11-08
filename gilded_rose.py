8dd20d2bfa49a1b53f3ccd2e7b083c59a1824189
Alexandru Dorobantu <alex.do.mihai@gmail.com>
2025-11-08 12:39:56 +0200

    fixed gilded rose

--- gilded_rose.py
+++ gilded_rose.py
@@ -4,37 +4,67 @@ class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
+        for it in self.items:
+            self.clip(it)
 
-    def update_quality(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+    def clip(self, it):
+        if it.name == "Sulfuras, Hand of Ragnaros":
+            return
+
+        if it.quality < 0:
+            it.quality = 0
+        elif it.quality > 50:
+            it.quality = 50
+
+    def handle_special_item(self, item) -> bool:
+        item.sell_in -= 1
+
+        # handle aged brie
+        if item.name == "Aged Brie":
+            item.quality += 1
+            return True
+        # handle passes
+        elif "Backstage passes" in item.name:
+            # apply rules for backstage passes quality
+            if item.sell_in <= 0:
+                item.quality = 0
+            elif item.sell_in <= 5:
+                item.quality += 3
+            elif item.sell_in <= 10:
+                item.quality += 2
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                item.quality += 1
 
+            return True
+        # handle sulfuras
+        elif item.name == "Sulfuras, Hand of Ragnaros":
+            item.sell_in += 1
+            return True
+        else:
+            return False
+
+    def handle_regular_item(self, item) -> None:
+        item.sell_in -= 1
+        expired_multiplier = 1
+
+        if item.sell_in  < 0:
+            expired_multiplier = 2
+        
+        if "Conjured" in item.name:
+            item.quality -= 2 * expired_multiplier
+        else:
+            item.quality -= 1 * expired_multiplier
+
+    def update_quality(self) -> None:
+        for it in self.items:
+            is_special = self.handle_special_item(it)
+
+            if is_special:
+                pass
+            else:
+                self.handle_regular_item(it)
+
+            self.clip(it)
 
 class Item:
     def __init__(self, name, sell_in, quality):
